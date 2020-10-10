99af9642802eaa7486b37e042cd5cf06169c7222
Hamed Masafi <hamed.masafi@gmail.com>
2020-10-10 19:55:26 +0330

    vuetify

--- src/components/ListManagement.vue
+++ src/components/ListManagement.vue
@@ -1,18 +1,36 @@
 <template>
-<section>
-    <div v-for="(item, i) in model" :key="i">
+<v-card elevation="10">
+            <v-card-text>
+
+    <h1>{{title}}</h1>
+    <v-row v-for="(item, i) in model" :key="i">
         <!-- <slot name="items"  :item="item"></slot> -->
+        <v-col cols="1" class="number-cell">
+        <span>{{i + 1}}</span>
+        </v-col>
+        <v-col cols="10">
         <slot :add="add" :remove="remove" :item="item" :index="i">
             <template v-for="(e, i) in names" >
                 {{e}}-{{i}} <input v-model="item[e]" :key="i" @change="add"/>
             </template>
         </slot>
-    </div>
-</section>
+        </v-col>
+        <v-col cols="1">
+        <v-btn @click="remove(i)" color="error">
+            <v-icon dark>
+                mdi-delete
+            </v-icon>
+        </v-btn>
+        </v-col>
+    </v-row>
+        </v-card-text>
+
+</v-card>
 </template>
 <script>
 export default {
     props:{
+        title: String,
         model: Array,
         names: Array
     },
@@ -22,6 +40,7 @@ export default {
             for (var p in this.names) {
                 var v = e[this.names[p]]
                 if (v === '' || v === null || v === undefined) {
+                    console.log('prop is null', this.names[p])
                     return;
                 }
                 console.log(p)
@@ -29,7 +48,9 @@ export default {
             this.model.push({})
         },
         remove(index) {
-            this.model.splice(index, 1)
+            var p = confirm("Are you sure to delete this item?") 
+            if (this.model.length > 1 && p)
+                this.model.splice(index, 1)
         }
     }
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="hello">
-    <ListManagement :names="['name', 'from', 'to']" :model="educations"  v-slot:default="{add, remove, item, index}">
-        <div slot="items">
-           Name: <input v-model="item.name" @change="add"/> 
-        <input v-model="item.from" @change="add"/> 
-        <input v-model="item.to" @change="add"/> 
+  <v-container class="main">
+    <section>
+      <v-text-field label="Name" dense v-model="contact_info.name" />
+      <v-text-field label="Last" dense v-model="contact_info.last_name" />
+      <v-text-field label="EMail" dense v-model="contact_info.last_name" />
+      <v-text-field label="GitHub address" dense v-model="contact_info.last_name" />
+    </section>
+    <ListManagement title="Educations" :names="['name', 'from', 'to']" :model="educations"  v-slot:default="{add, item}">
+        <v-row slot="items">
+          <v-col>
+            <v-text-field dense v-model="item.name" @change="add"/> 
+          </v-col>
+          <v-col>
+            <v-text-field dense v-model="item.from" @change="add"/> 
+          </v-col>
+          <v-col>
+            <v-text-field dense v-model="item.to" @change="add"/> 
+          </v-col>
+        </v-row>
+    </ListManagement>
 
-        <button @click="remove(index)">x</button>
+    <!-- <ListManagement title="skills" :names="['title', 'skill', 'years']" :model="skills"  v-slot:default="{add, item}">
+        <div slot="items"> 
+          <v-text-field dense v-model="item.title" @change="add"/> 
+          <select v-model="item.skill" @change="add"> 
+            <option>Beginner</option>
+            <option>Midlevel</option>
+            <option>Junior</option>
+          </select>
+          <v-text-field dense type="number" v-model="item.years" @change="add"/> 
         </div>
+    </ListManagement> -->
+
+    <ListManagement title="Skills" :names="['title']" :model="skills"  v-slot:default="{item, add}">
+        <v-text-field label="Skill group name" dense v-model="item.title" @change="add"/> 
+        <ListManagement :title="item.title" :names="['title', 'skill', 'years']" :model="item.subs"  v-slot:default="{add, item}">
+          <v-row slot="items">
+            <v-col>
+              <v-text-field label="Title" dense v-model="item.title" @change="add"/> 
+            </v-col>
+            <v-col>
+              <v-select dense v-model="item.skill" @change="add" :items="['Beginner', 'Advanced']"> 
+              </v-select>
+            </v-col>
+            <v-col>
+              <v-text-field label="Years" dense type="number" v-model="item.years" @change="add"/> 
+            </v-col>
+          </v-row>
+      </ListManagement>
     </ListManagement>
+
+    <ListManagement title="Open source projects" :names="['title', 'descript', 'url']" :model="opensource_projects"  v-slot:default="{add, item}">
+          <v-row slot="items">
+            
+            <v-col>
+              <v-text-field dense label="Title" v-model="item.title" @change="add"/> 
+            </v-col>
+            <v-col>
+              <v-text-field dense label="Descript" v-model="item.descript" @change="add"/> 
+            </v-col>
+            <v-col>
+              <v-text-field dense label="Url" v-model="item.url" @change="add"/> 
+            </v-col>
+          </v-row>
+      </ListManagement>
+
+      <div>
+        <v-btn>Save</v-btn>
+      </div>
     ==============
-    <ListManagement :names="['name', 'from', 'to']" :model="educations">
-    </ListManagement>
-  </div>
+    <!-- <ListManagement :names="['name', 'from', 'to']" :model="educations">
+    </ListManagement> -->
+    {{skills}}
+  </v-container>
 </template>
 
 <script>
@@ -25,8 +85,12 @@ export default {
   },
   data () {
     return {
-    drawer: null,
-    educations: [{}]
+    contact_info: {},
+    educations: [{}],
+    opensource_projects: [{}],
+    skills: [{
+      subs: [{}]
+    }],
     }
   }
 }
